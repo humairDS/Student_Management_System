543af310ec3a93b40834993653815ae4b56fa3ab
humairDS <humairbashiryousufi@gmail.com>
2025-09-07 19:53:28 +0500

    student_details.py

--- student_details.py
+++ student_details.py
@@ -1,16 +1,12 @@
-class student:    #class
+class Student:
+    def __init__(self, name, roll_no, marks):
+        self.name = name
+        self.roll_no = roll_no
+        self.marks = marks
 
-    def __init__(self,name,roll_no,marks):   #constructor/initializer
-
-        self.name = name   #Instance Attributes     
-        self.roll_no = roll_no #Instance Attributes               
-        self.marks = marks #Instance Attributes
-
-
-    def student_det(self):  #Instance Method
-        print(f"Name:{self.name} Roll Number:{self.roll_no} Marks:{self.marks}")
-
-student_details = student("umair",23,87)  #Object
-
-
-student_details.student_det()  #calling method to display details
+    def student_det(self):
+        return {
+            "Name": self.name,
+            "Roll Number": self.roll_no,
+            "Marks": self.marks
+        }
